93b9e9952251f73e7b9e58cc9a55c90ed55ce2f3
Ernest Pazera <ernestpazera@gmail.com>
2023-01-02 04:15:10 -0600

    quitting! the game!

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # PyramidOfAhraxis
 https://watabou.github.io/one-page-dungeon/?seed=1653868650
+
+## Log
+
+* 01: 20230102
--- src/PyramidOfAhraxis.cbl
+++ src/PyramidOfAhraxis.cbl
@@ -4,7 +4,41 @@ PROGRAM-ID. PYRAMIDOFAHRAXIS.
 
 DATA DIVISION.
        WORKING-STORAGE SECTION.
-
+       01 SCRATCHPAD.
+          02 COMMAND PIC X.
+                       
 PROCEDURE DIVISION.
-       DISPLAY "PYRAMID OF AHRAXIS"
-STOP RUN.
+TITLESCREEN.
+       DISPLAY SPACE
+       DISPLAY "************************************************************"
+       DISPLAY "*                                                          *"
+       DISPLAY "*                    PYRAMID OF AHRAXIS                    *"
+       DISPLAY "*                                                          *"
+       DISPLAY "*            A PRODUCTION OF THEGRUMPYGAMEDEV              *"
+       DISPLAY "*            FOR INTERACTIVE FICTION GAME JAM              *"
+       DISPLAY "*                                                          *"
+       DISPLAY "************************************************************"
+       DISPLAY SPACE
+       DISPLAY "[S]TART GAME"
+       DISPLAY "[I]NSTRUCTIONS"
+       DISPLAY "[Q]UIT"
+       ACCEPT COMMAND
+       EVALUATE COMMAND
+        WHEN "s" WHEN "S"
+        WHEN "i" WHEN "I"
+        WHEN "q" WHEN "Q"
+           GO TO CONFIRMQUIT
+        WHEN OTHER 
+           GO TO TITLESCREEN
+       END-EVALUATE.
+
+CONFIRMQUIT.
+       DISPLAY SPACE 
+       DISPLAY "ARE YOU SURE YOU WANT TO QUIT?"
+       DISPLAY "[Y]ES"
+       DISPLAY "[N]O"
+       ACCEPT COMMAND
+       IF COMMAND="y" OR COMMAND="Y" THEN 
+           STOP RUN
+       END-IF
+       GO TO TITLESCREEN.
